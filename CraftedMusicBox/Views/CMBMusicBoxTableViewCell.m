9b46bfec9df9d8837810a3c946c265724f2a7560
hidetaka-matsumoto <hide@yari.local>
2014-11-30 13:16:13 +0900

    - うぐいすデザインを実装

--- CraftedMusicBox/Views/CMBMusicBoxTableViewCell.m
+++ CraftedMusicBox/Views/CMBMusicBoxTableViewCell.m
@@ -75,7 +75,7 @@
 - (void)process
 {
 //    NSLog(@"process y=%f", self.frame.origin.y - _parentTableView.contentOffset.y);
-    CGFloat tineY = _tineView.frame.origin.y - 20.0f; // StatusBar分
+    CGFloat tineY = _tineView.frame.origin.y + _tineView.frame.size.height - 20.0f; // StatusBar分
     CGFloat curY = self.frame.origin.y - _parentTableView.contentOffset.y;
     if (tineY >= curY && tineY < _preY) {
         NSLog(@"process tineY=%f, curY=%f, preY=%f", tineY, curY, _preY);
